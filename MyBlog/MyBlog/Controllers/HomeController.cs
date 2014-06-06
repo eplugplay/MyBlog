67177eb3822a1a8a45f327b8b13cfc5cd56b080d
Steve Yi <steveyi32@gmail.com>
2014-06-06 16:20:47 -0500

    added new image in homepage as well as a horizontal line to space out each posts.

--- MyBlog/MyBlog/Controllers/HomeController.cs
+++ MyBlog/MyBlog/Controllers/HomeController.cs
@@ -11,12 +11,8 @@ namespace MyBlog.Controllers
 {
     public class HomeController : Controller
     {
-        MyBlog.Models.MyBlog _myblog = new Models.MyBlog();
         public ActionResult Index()
         {
-            //ViewBag.Message = "Welcome to my blog.";
-            //_myblog.test = "finally!";
-            //return View(_myblog);
             return View();
         }
 
