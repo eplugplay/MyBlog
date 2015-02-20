b9809704435aa001ed358ae30e9adc9ca83ed27c
Steve Yi <steveyi32@gmail.com>
2015-02-20 15:21:03 -0600

    changed message.

---
+++ MyBlog/MyBlog/Scripts/Msg.js
@@ -0,0 +1,26 @@
+﻿function LoadMsg(msg) {
+    $("#popupwindow").dialog({
+        autoOpen: false,
+        modal: true,
+        show: "fade",
+        title: "Information",
+        buttons: {
+            OK: function () { $(this).dialog("close"); }
+        }
+    });
+    $('#popupwindow').html(msg);
+    $("#popupwindow").dialog("open");
+}
+function LoadSecondaryMsg(msg) {
+    $("#popupwindow2").dialog({
+        autoOpen: false,
+        modal: true,
+        show: "fade",
+        title: "Information",
+        buttons: {
+            OK: function () { $(this).dialog("close"); }
+        }
+    });
+    $('#popupwindow2').html(msg);
+    $("#popupwindow2").dialog("open");
+}
